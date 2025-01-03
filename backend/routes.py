0655da5b9f3a1da20add31789763768881486939
Nikola Socec <nikola.socec@gmail.com>
2025-01-03 14:59:10 +0100

    Added backend skeleton.

---
+++ backend/routes.py
@@ -0,0 +1,13 @@
+import typing
+from typing import List, Union
+
+from fastapi import APIRouter, Response
+from fastapi.requests import Request
+from fastapi.responses import JSONResponse
+
+api_router = APIRouter()
+
+
+@api_router.get("/")
+async def placeholder() -> JSONResponse:
+    return {"hello": "world"}
