0337c8faa3cee0c56d94aa9f0b47fae2f48674b7
Nikola Socec <nikola.socec@gmail.com>
2025-01-26 20:17:34 +0100

    Fixed cost filtering.

--- backend/entities/dynamic/units/worker.py
+++ backend/entities/dynamic/units/worker.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Type
 
 from entities.dynamic.buildings.building import Building
 from entities.dynamic.buildings.resource_collectors.farm import Farm
@@ -21,7 +21,7 @@ def get_all_other_buildings() -> List[Building]:
         if (
                 key.startswith("buildings")
                 and "resource_collectors" not in key
-                and "capital" not in key
+                and "Capital" not in key
         ):
             found_buildings.append(module)
 
@@ -39,6 +39,54 @@ class Worker(Unit, ConcreteEntity):
     def calculate_available_buildings(
             self, game
     ) -> List[List[str]]:  # (namespace, type) tuples
+        buildings = self._calculate_available_buildings_by_position(game)
+        buildings = self._filter_available_buildings_by_cost(buildings, game)
+
+        return [
+            [building.get_namespace(), building.get_type()]
+            for building in buildings
+        ]
+
+    def build(self, namespace: str, type: str, game) -> Building:
+        from utils.registry import registry
+
+        try:
+            building_cls = registry.get(type, namespace)
+        except ValueError:
+            raise ValueError("Building does not exist")
+
+        building_instance = building_cls(
+            position=self.position, teamId=self.teamId,
+        )
+
+        buildings = self._calculate_available_buildings_by_position(game)
+
+        if building_cls not in buildings:
+            raise ValueError("Building not allowed to be built here")
+
+        current_team = game.teams[self.teamId]
+
+        if current_team.resources.can_afford(
+                self._get_building_cost(building_cls)
+        ):
+            cost = Resources(
+                food=building_instance.cost[0],  # type: ignore
+                wood=building_instance.cost[1],  # type: ignore
+                minerals=building_instance.cost[2],  # type: ignore
+            )
+
+            game.teams[self.teamId].resources.pay(cost)
+
+            game.map.add_entity(building_instance)
+            game.map.remove_entity(self)
+        else:
+            raise ValueError("Not enough resources to build")
+
+        return building_instance  # type: ignore
+
+    def _calculate_available_buildings_by_position(
+            self, game
+    ) -> List[Type[Building]]:
         resource_buildings = []
         other_buildings = []
 
@@ -57,34 +105,31 @@ class Worker(Unit, ConcreteEntity):
         ):
             other_buildings = get_all_other_buildings()
 
-        return [
-            [building.get_namespace(), building.get_type()]
-            for building in resource_buildings + other_buildings
-        ]
+        return resource_buildings + other_buildings  # type: ignore
 
-    def build(self, namespace: str, type: str, game) -> Building:
-        from utils.registry import registry
+    def _filter_available_buildings_by_cost(
+            self, available_buildings: List[Type[Building]], game
+    ) -> List[Type[Building]]:
+        current_team = game.teams[self.teamId]
+        filtered_buildings = []
 
-        building_cls = registry.get(type, namespace)
-        building_instance = building_cls(
-            position=self.position, teamId=self.teamId,
+        for building in available_buildings:
+            if current_team.resources.can_afford(
+                    self._get_building_cost(building)
+            ):
+                filtered_buildings.append(building)
+
+        return filtered_buildings
+
+    def _get_building_cost(
+            self, building: Type[Building],
+    ) -> Resources:
+        building_instance = building(
+            position=self.position, teamId=self.teamId
         )
 
-        if [namespace, type] not in self.calculate_available_buildings(game):
-            raise ValueError("Building not allowed to be built here")
-
-        cost = Resources(
+        return Resources(
             food=building_instance.cost[0],  # type: ignore
             wood=building_instance.cost[1],  # type: ignore
             minerals=building_instance.cost[2],  # type: ignore
         )
-
-        if game.teams[self.teamId].resources.can_afford(cost):
-            game.teams[self.teamId].resources.pay(cost)
-
-            game.map.add_entity(building_instance)
-            game.map.remove_entity(self)
-        else:
-            raise ValueError("Not enough resources to build")
-
-        return building_instance  # type: ignore
