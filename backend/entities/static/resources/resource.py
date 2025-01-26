3d67939cb8f0eef90d9ef52120fbe8a21f427c99
Nikola Socec <nikola.socec@gmail.com>
2025-01-26 21:37:06 +0100

    Assigned neutral team to resources and added map with full functionality.

--- backend/entities/static/resources/resource.py
+++ backend/entities/static/resources/resource.py
@@ -1,7 +1,10 @@
+from consts import TEAMS_NEUTRAL_ID
 from entities.entity import Entity
 
 
 class Resource(Entity):
+    teamId: str = TEAMS_NEUTRAL_ID
+
     @classmethod
     def get_namespace(cls) -> str:
         return "resources"
